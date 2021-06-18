870c53d749cacf044866ce351893a98bf8dc3a3e
Luiz Thiago <thiago_fers7@hotmail.com>
2021-06-18 19:52:02 -0300

    create Context

--- desafio/src/App.jsx
+++ desafio/src/App.jsx
@@ -1,18 +1,20 @@
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import { HomeContextProvider } from './contexts/HomeContext'
 
 import styles from './styles/app.module.css'
 import './styles/global.css'
 
 function App() {
   return (
-    <div className={styles.appContainer}>
-      <Header />
-
-      <main>
-        <Home />
-      </main>
-    </div>
+    <HomeContextProvider>
+      <div className={styles.appContainer}>
+        <Header />
+        <main>
+          <Home />
+        </main>
+      </div>
+    </HomeContextProvider>
   )
 }
 
--- desafio/src/components/Home/Home.jsx
+++ desafio/src/components/Home/Home.jsx
@@ -1,4 +1,10 @@
+import { useContext } from 'react'
+import { HomeContext } from '../../contexts/HomeContext'
+
+
 function Home() {
+    const { } = useContext(HomeContext)
+
     return (
         <section>
             <p>Capa 1</p>
