dd188f0475a7716eb9bba6bb5cd218d4a3bdc794
Luiz Thiago <thiago_fers7@hotmail.com>
2021-06-18 21:45:58 -0300

    resquest

--- desafio/src/components/Home/Home.jsx
+++ desafio/src/components/Home/Home.jsx
@@ -1,13 +1,37 @@
-import { useContext } from 'react'
-import { HomeContext } from '../../contexts/HomeContext'
-
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
 
 function Home() {
-    const { } = useContext(HomeContext)
+    const [homeEpisodes, setHomeEpisodes] = useState([])
+    const [isEpisode, setIsEpisode] = useState(false)
+
+    useEffect(() => {
+        api.get('top/anime/1/upcoming')
+            .then(({ data }) => {
+                setHomeEpisodes(data.top)
+                setIsEpisode(true)
+            })
+            .catch(error => {
+                setIsEpisode(false)
+            })
+    }, [])
 
     return (
         <section>
-            <p>Capa 1</p>
+            {isEpisode && (
+                homeEpisodes.map(episode => {
+                    return (
+                        <div key={episode.mal_id}>
+                            <img src={episode.image_url} alt="Capa" />
+                            <div>
+                                <button type="button">X</button>
+                                <strong>{episode.title}</strong>
+                                <span>Data de lançamento: {episode.start_date}</span>
+                            </div>
+                        </div>
+                    )
+                })
+            )}
         </section>
     )
 }
--- desafio/src/contexts/HomeContext.jsx
+++ desafio/src/contexts/HomeContext.jsx
@@ -1,11 +1,14 @@
 import { createContext } from 'react'
 
+
 export const HomeContext = createContext({})
 
 export function HomeContextProvider({ children }) {
+
+
     return (
         <HomeContext.Provider value={{
-            
+
         }}>
             {children}
         </HomeContext.Provider>
